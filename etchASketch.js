39ee231def5e114a9df1e98b5d5d6f836851892b
Ben <ben.d.godfrey2002@gmail.com>
2024-08-13 15:57:22 +0100

    Javascript adds square to DOM

---
+++ etchASketch.js
@@ -0,0 +1,20 @@
+let etchASketch = document.querySelector("#etchASketch");
+
+for (let row = 0; row < 16; row++){
+    let rowDiv = document.createElement("div");
+    rowDiv.setAttribute("class", "row");
+
+    rowDiv.style.display = "flex";
+    rowDiv.style.flexDirection = "row";
+
+    for (let col = 0; col < 16; col++){
+        let cellDiv = document.createElement("div");
+        cellDiv.setAttribute("class", "cell");
+        rowDiv.appendChild(cellDiv);
+
+        cellDiv.style.width = "50px";
+        cellDiv.style.height = "50px";        
+    }
+
+    etchASketch.appendChild(rowDiv);
+}
